feb4e11f5629c4cd5fece215db67aa84a22fb88b
Luiz Picolo <luizpicolo@gmail.com>
2018-10-03 15:36:28 -0400

    Removido código desnecessário

--- spec/tads6_picolo_spec.rb
+++ spec/tads6_picolo_spec.rb
@@ -2,9 +2,4 @@ RSpec.describe Tads6Picolo do
   it "has a version number" do
     expect(Tads6Picolo::VERSION).not_to be nil
   end
-
-  it "Deveria mostrar as palavras Hello World" do
-    x = Tads6Picolo::HelloWorld.new
-    expect(x.say("Hello World")).to eq("Hello World")
-  end
 end
